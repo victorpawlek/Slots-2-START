8abc0de11fae662f58fa278f97e23a3142a25a1b
victorpawlek <pawlek.v03@htlwienwest.at>
2022-01-16 22:18:23 +0100

    fertig

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container mt-5" id="app">
+    <Catogram :cats="cats">
+      <template #boss="{item}">
+        <h5>{{ item.name }}</h5>
+        <img :src="item.image" width="200" alt="" />
+      </template>
+      <template #employees="{item}">
+        
+          <h5>{{ item.name }}</h5>
+          <img :src="item.image" width="200" alt="" />
+          <p class="mt-2 fw-bold">Job role: {{ item.role }}</p>
+        
+      </template>
+    </Catogram>
+  </div>
+</template>
+
+<script>
+import cats from '@/assets/cats.json';
+
+import Catogram from '@/components/Catogram.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Catogram,
+  },
+  data() {
+    return {
+      cats: cats
+      // [
+      //   { name: 'Jermy', role: 'Boss', img: 'images/jermy.jpg' },
+      //   { name: 'Franz', role: 'Sales', img: 'images/franz.jpg' },
+      //   { name: 'Pizz', role: 'Research', img: 'images/pizz.jpg' },
+      //   { name: 'Stella', role: 'Customer Support', img: 'images/stella.jpg' },
+      // ],
+    };
+  },
+};
+</script>
---
+++ src/components/Catogram.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- Add your code here -->
+    <div>
+      <h3>Boss</h3>
+      <slot name="boss" :item="boss"></slot>
+    </div>
+    <div>
+      <h3>Employees</h3>
+        <div class="d-flex flex-row">
+          <div class="m-2" v-for="(employee,i) in employees" :key="i"><slot name="employees" :item="employee"></slot></div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cats: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      boss: this.cats.find((el) => el.role == 'Boss'),
+      employees: this.cats.filter((el) => el.role != 'Boss'),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
